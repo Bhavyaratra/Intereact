83d25b9e32536635e5f6d4b7f2d933f6d1ad9252
Bhavyaratra <bhavya.r1220@gmail.com>
2022-03-31 16:36:09 +0530

    added hosted server

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,13 @@ import { Route, Switch } from 'react-router-dom';
 //components
 import Editor from './Components/Editor/Editor';
 import Home from './Components/Home/Home';
-import Room from './Components/views/ViewRoom';
+import ViewRoom from './Components/views/Room.js';
 
 function App() {
   return (<>
       <Switch>
         <Route exact path="/editor" component={Editor} />
-        <Route path="/room" component={Room} />
+        <Route path="/room" component={ViewRoom} />
         <Route path="/" component={Home} />
         
       </Switch>
--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,5 @@
 import * as io from "socket.io-client";
 
-const socket = io('http://localhost:8000');
+const socket = io(process.env.REACT_APP_SERVER_URL);
 console.log(socket);
 export default socket;
